63c72e471a93a3ba73f5c0fb6b0ddaa2c721ca98
Yannick Debrée <yannickdebree@laposte.net>
2023-04-10 15:59:12 +0200

    Architecture reviewed

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    root: 'src',
     build: {
-        outDir: '../dist',
-        emptyOutDir: true
-    }
+        emptyOutDir: true,
+        outDir: '../dist'
+    },
+    root: 'src'
 })
