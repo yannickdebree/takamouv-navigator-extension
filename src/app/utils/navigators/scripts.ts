6a51e68d7c72e1742bd01c8b786d32df027fb292
Yannick Debrée <yannickdebree@laposte.net>
2023-04-11 23:15:49 +0200

    Add wait time during autocomplete

--- src/app/utils/navigators/scripts.ts
+++ src/app/utils/navigators/scripts.ts
@@ -18,33 +18,42 @@ export function autocompleteTrainingForm(dancerInformations: DancerInformations)
         }
     }
 
+    const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
     const setCountryAsDefaultValue = () => {
         const defaultCountryListItemQuerySelector = '.nice-select.country.formitem [data-value="66"]';
         const defaultCountryListItem = document.querySelector<HTMLLIElement>(defaultCountryListItemQuerySelector);
         if (!defaultCountryListItem) {
-            return new MissingDomElementError(defaultCountryListItemQuerySelector);
+            throw new MissingDomElementError(defaultCountryListItemQuerySelector);
         }
         defaultCountryListItem.click();
+        return Promise.resolve();
     };
 
     const setStatusAsDefaultValue = () => {
         const statusInputQuerySelector = '[for="P2_PERSONNEPHYSIQUE_0"]';
         const statusInputElement = document.querySelector<HTMLLabelElement>(statusInputQuerySelector);
         if (!statusInputElement) {
-            return new MissingDomElementError(statusInputQuerySelector);
+            throw new MissingDomElementError(statusInputQuerySelector);
         }
         statusInputElement.click();
+        return Promise.resolve();
     }
 
-    const setFieldsFromUserData = () => {
+    const timeOut = 100;
+
+    const setFieldsFromUserData = async () => {
         for (let querySelector of Object.keys(inputElementsMapping)) {
             const inputElement = document.querySelector<HTMLInputElement>(querySelector);
 
             if (!inputElement) {
-                return new MissingDomElementError(querySelector);
+                throw new MissingDomElementError(querySelector);
             }
 
             inputElement.value = (dancerInformations as any)[inputElementsMapping[querySelector]];
+            inputElement.dispatchEvent(new Event("change"))
+
+            await wait(timeOut);
         }
     }
 
@@ -52,36 +61,34 @@ export function autocompleteTrainingForm(dancerInformations: DancerInformations)
         const mrsInputQuerySelector = '[for="F04_4_1_0"]';
         const mrsInputElement = document.querySelector<HTMLLabelElement>(mrsInputQuerySelector);
         if (!mrsInputElement) {
-            return new MissingDomElementError(mrsInputQuerySelector);
+            throw new MissingDomElementError(mrsInputQuerySelector);
         }
 
         const mrInputQuerySelector = '[for="F04_4_1_1"]';
         const mrInputElement = document.querySelector<HTMLLabelElement>(mrInputQuerySelector);
         if (!mrInputElement) {
-            return new MissingDomElementError(mrsInputQuerySelector);
+            throw new MissingDomElementError(mrsInputQuerySelector);
         }
 
         if (dancerInformations.civility === "mrs") {
             mrsInputElement.click();
-            return;
+            return Promise.resolve();
         }
         mrInputElement.click();
+        return Promise.resolve();
     }
 
-    const wrapActions = (actions: Array<() => Error | undefined>) => {
-        let result: Error | undefined;
+    const wrapActions = async (actions: Array<() => Promise<void>>) => {
         for (const action of actions) {
-            result = action();
-            if (result instanceof Error) {
-                return result;
-            }
+            await action();
+            await wait(timeOut);
         }
     }
 
     return wrapActions([
         setCountryAsDefaultValue,
         setStatusAsDefaultValue,
-        setFieldsFromUserData,
-        setCivilityFromUserData
+        setCivilityFromUserData,
+        setFieldsFromUserData
     ]);
 }
